637b18d4773bf9380b067ac6a8a2b96f7e4bd177
chinmayee <cvwaingankar@gmail.com>
2020-06-15 13:18:33 +0530

    First Commit

---
+++ precache-manifest.e3867edd495e01ec1d97abbcee7d252c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620f4a38097791b76c357db2b7cb6a89",
+    "url": "/chinmayee212/reacthooked/index.html"
+  },
+  {
+    "revision": "14f8f0e942f7bf5bf1a8",
+    "url": "/chinmayee212/reacthooked/static/css/main.da621410.chunk.css"
+  },
+  {
+    "revision": "e926a947108d74c43933",
+    "url": "/chinmayee212/reacthooked/static/js/2.9c3a92ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chinmayee212/reacthooked/static/js/2.9c3a92ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14f8f0e942f7bf5bf1a8",
+    "url": "/chinmayee212/reacthooked/static/js/main.192900ae.chunk.js"
+  },
+  {
+    "revision": "cb3ccc8acfc8acadf154",
+    "url": "/chinmayee212/reacthooked/static/js/runtime-main.2cfcff90.js"
+  }
+]);
